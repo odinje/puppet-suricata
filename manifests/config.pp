1c01aabcda7f6bead29d26675128e549860de358
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2016-11-11 22:04:35 +0100

    Adding suricata user and group

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,25 @@
 class suricata::config {
   
+  user { 'suricata':
+    ensure  => present,
+    system  => true,
+    shell   => '/sbin/nologin',
+    gid     => 'suricata',
+    comment => 'Suricata Open Source IDS / IPS / NSM engine',
+    require => Group['suricata'],
+  }
+  group { 'suricata':
+    ensure => present,
+    system => true,
+  }
+
   file { "${::suricata::config_dir}/suricata.yaml":
     ensure  => file,
     owner   => 'suricata',
     group   => 'root',
     mode    => '0600',
     content => epp('suricata/suricata.yaml.epp'),
+    notify  => Service['suricata'],
+    require => User['suricata'],
   }
 }
