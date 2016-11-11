37cee56c1dc8304d8d96bc36b5352b3ce389267b
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2016-11-11 19:06:56 +0100

    Adding tests for checking config file

--- spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'suricata' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('suricata') }
-  end
-end
---
+++ spec/classes/suricata_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+describe 'suricata' do
+
+  let(:facts) { {
+    :osfamily => 'RedHat',
+
+  } }
+   
+ 
+
+
+  context 'with defaults for all parameters' do
+    it { should contain_class('suricata') }
+  end
+
+  it { should contain_package('suricata').with_ensure('present') }
+
+  it do
+    is_expected.to contain_file('/etc/suricata/suricata.yaml').with({
+      'ensure' => 'file',
+      'owner'  => 'suricata',
+      'group'  => 'root',
+      'mode'   => '0600',
+      })
+  end 
+end
