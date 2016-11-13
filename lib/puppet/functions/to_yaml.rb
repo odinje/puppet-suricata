3aa26aee05ff60e5cee28bd2968922620f39e53b
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2016-11-13 20:33:44 +0100

    Adding array to yaml functionality

--- lib/puppet/functions/to_yaml.rb
+++ lib/puppet/functions/to_yaml.rb
@@ -1,13 +1,20 @@
 require 'yaml'
 
 Puppet::Functions.create_function(:to_yaml) do
-  dispatch :to_yaml do
+  dispatch :hash_to_yaml do
     param 'Hash', :hash
   end
 
-  def to_yaml(hash)
+  dispatch :array_to_yaml do
+    param 'Array', :array
+  end
+
+  def hash_to_yaml(hash)
     hash.to_yaml
   end
 
+  def array_to_yaml(array)
+    array.to_yaml
+  end
 end
 
