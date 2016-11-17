9faca4d57bf6547b9bdc4ee05623bb4e0b644f20
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2016-11-17 14:33:15 +0100

    Removing epel dependency, and installing it from yum

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,10 +2,13 @@ class suricata::install {
 
   case $::osfamily {
     'RedHat': {
+      package { 'epel-release':
+        ensure => installed,
+      }
       package { 'suricata':
         ensure  => $::suricata::ensure,
         name    => $::suricata::package_name,
-        require => Class['::epel'],
+        require => Package['epel-release'],
       }
     }
     default: {
