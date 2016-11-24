265b07692bf8375d1f1abb2addea929a36d55a89
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2016-11-24 20:46:05 +0100

    Remove comments from unit tests

--- spec/classes/suricata_spec.rb
+++ spec/classes/suricata_spec.rb
@@ -22,13 +22,6 @@ describe 'suricata' do
         'shell'  => '/sbin/nologin',
         'gid'    => 'suricata',
        )}
-      #it 'should set up /etc/suricata/suricata.yaml as concat resource' do
-      #  expect(subject).to contain_concat('/etc/suricata/suricata.yaml').with(
-      #    'owner' => 'suricata',
-      #    'group' => 'root',
-      #    'mode'  => '0600' 
-      #  )
-      #end
 
       it { should contain_file('/etc/suricata/classification.config').with(
         'ensure' => 'file',
@@ -55,14 +48,14 @@ describe 'suricata' do
       end
 
 
-  it do
-    is_expected.to contain_file('/etc/suricata/suricata.yaml').with({
-      'ensure' => 'present',
-      'owner'  => 'suricata',
-      'group'  => 'root',
-      'mode'   => '0600',
-      })
-  end 
+      it do
+        is_expected.to contain_file('/etc/suricata/suricata.yaml').with({
+          'ensure' => 'present',
+          'owner'  => 'suricata',
+          'group'  => 'root',
+          'mode'   => '0600',
+          })
+      end 
     end
   end
 end
