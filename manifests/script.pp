1da576fa8c7c3f8d6216d8f9be8a48d4a608fd16
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2017-01-04 23:11:54 +0100

    Script resource for adding suricata scripts

--- manifests/script.pp
+++ manifests/script.pp
@@ -1,7 +1,18 @@
 define suricata::script (
-  String $ensure = 'present',
-  String $name   = $title,
   Pattern[/^puppet/] $source,
+  Enum['present','absent'] $ensure = 'present',
+  String $script_name   = $title,
 ) {
-}
 
+  if ! defined(Class['suricata']) {
+    fail('You must include the suricata base class before using any suricata defined resources')
+  }
+
+  file { "${::suricata::config_dir}/lua-output/${script_name}":
+    ensure => $ensure,
+    owner  => $::suricata::user,
+    group  => 'root',
+    mode   => '0600',
+    source => $source,
+  }
+}
